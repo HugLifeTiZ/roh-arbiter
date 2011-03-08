2b6cf2821c635a771ce78c4aa64a607dfd4f97f4
TwilightInZero <TwilightInZero@bbc52062-dd48-11de-b878-c5ec750a8c44>
2011-03-08 22:28:42 +0000

    - Migrated to waf 1.6.3. - Updated Mono URL for Mac OSX in readme. - Updated copyright year on all source files.

--- src/Combatant.cs
+++ src/Combatant.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Trent McPheron <twilightinzero@gmail.com>
 // 
-// Copyright (c) 2010 Trent McPheron
+// Copyright (c) 2010-2011 Trent McPheron
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
